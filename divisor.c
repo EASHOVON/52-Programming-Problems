ed0a84fc56fe2f6f4bad72b8899b04cbe462c9c7
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-06-27 23:31:03 +0600

    fix return statement of falling Number Problem and completed divisor problem

---
+++ divisor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+    int T,N;
+    scanf("%d", &T);
+    for (int i = 1; i <=T;i++)
+    {
+        scanf("%d", &N);
+        printf("Case %d: ",i);
+        for (int j = 1; j <= N; j++)
+        {
+            if(N%j==0){
+                printf("%d ",j);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
